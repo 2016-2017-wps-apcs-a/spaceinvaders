4de8e8bd1e3271ff2b908450525f2be9cd79099c
wps-dpetty <dpetty@winchesterps.org>
2017-05-27 01:19:26 -0400

    Rearranged and renamed files to go into the spaceinvaders package. Updated pom.xml to include javadoc & hamcrest.

---
+++ src/main/java/spaceinvaders/SpaceInvaders.java
@@ -0,0 +1,12 @@
+package spaceinvaders;
+
+/**
+ * SpaceInvaders
+ */
+public class SpaceInvaders
+{
+    public static void main(String[] args)
+    {
+        System.out.println("# SpaceInvaders");
+    }
+}
