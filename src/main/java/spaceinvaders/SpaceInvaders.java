b05db465a3ff71d6625ac58ff3f8a2d2f1d68ebc
wps-dpetty <dpetty@winchesterps.org>
2017-05-28 21:11:41 -0400

    Added javadoc comments to SpaceInvaders.java. Added commons-cli to pom.

--- src/main/java/spaceinvaders/SpaceInvaders.java
+++ src/main/java/spaceinvaders/SpaceInvaders.java
@@ -1,12 +1,21 @@
+/*
+ * SpaceInvaders.java
+ */
 package spaceinvaders;
 
 /**
- * SpaceInvaders
+ * SpaceInvaders is the main class for the SpaceInvaders game.
+ *
+ * @author David C. Petty
  */
-public class SpaceInvaders
-{
-    public static void main(String[] args)
-    {
+public class SpaceInvaders {
+
+    /**
+     * The SpaceInvaders main method that initializes the entire game.
+     *
+     * @param args command-line argument array
+     */
+    public static void main(String[] args) {
         System.out.println("# SpaceInvaders");
     }
 }
--- src/test/java/spaceinvaders/SpaceInvadersTest.java
+++ src/test/java/spaceinvaders/SpaceInvadersTest.java
@@ -1,4 +1,4 @@
-package org.winchesterps;
+package spaceinvaders;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
