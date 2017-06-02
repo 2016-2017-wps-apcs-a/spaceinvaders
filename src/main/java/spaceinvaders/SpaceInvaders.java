20c9f6f8ef408a3839ec92c62ca59e5996f9063a
Benny Liang <Ben130405@gmail.com>
2017-06-02 08:19:24 -0400

    Keyboard Commands

--- src/main/java/spaceinvaders/SpaceInvaders.java
+++ src/main/java/spaceinvaders/SpaceInvaders.java
@@ -9,13 +9,12 @@ package spaceinvaders;
  * @author David C. Petty
  */
 public class SpaceInvaders {
-
     /**
      * The SpaceInvaders main method that initializes the entire game.
      *
      * @param args command-line argument array
      */
     public static void main(String[] args) {
-        System.out.println("# SpaceInvaders!!!");
+        System.out.println("# SpaceInvaders");
     }
 }
