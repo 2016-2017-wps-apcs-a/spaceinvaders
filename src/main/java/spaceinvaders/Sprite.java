8ea638dafd5042b32939f1a03f76246a119bb751
wps-dpetty <dpetty@winchesterps.org>
2017-06-07 08:56:18 -0400

    Initial commit of FilledPolygon.java & SpaceInvaders.java.

---
+++ src/main/java/spaceinvaders/Sprite.java
@@ -0,0 +1,32 @@
+/*
+ * Sprite.java
+ */
+package spaceinvaders;
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+
+/**
+ * Sprite is a JComponent base class for all on-screen items.
+ *
+ * @author David C. Petty
+ */
+public class Sprite extends JComponent {
+
+    /** The list of <code>FilledPolygons</code> that define the sprite. */
+    private java.util.List<FilledPolygon> polys = new ArrayList<FilledPolygon>();
+
+    public Sprite() {
+        polys.add(new FilledPolygon(Color.RED,
+                                    new int[] { 685, 784, 784, 685, },
+                                    new int[] { 720, 720, 779, 779, }));
+        polys.add(new FilledPolygon(Color.GRAY,
+                                    new int[] { 685, 704, 704, 685, },
+                                    new int[] { 695, 695, 744, 744, }));
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        for (FilledPolygon poly : polys) { poly.draw(g); }
+    }
+}
