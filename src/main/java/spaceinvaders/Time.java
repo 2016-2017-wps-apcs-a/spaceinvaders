655260faaade2e1e434f5903db911ae2cb211318
Deniz Guler <TheToasterOfDoom1@gmail.com>
2017-06-13 07:52:54 -0400

    Added Time class that counts in ticks.

--- src/main/java/spaceinvaders/Time.java
+++ src/main/java/spaceinvaders/Time.java
@@ -4,6 +4,23 @@ package spaceinvaders;
  * @author Chris Callahan
  * */
 
-public class Time {
-	
+import javax.swing.Timer;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Time implements ActionListener {
+    private final int TICK_RATE = 20; // 1 s = 20 tick
+    private  long currentTick;
+    public Time() {
+        Timer timer = new Timer(TICK_RATE, this);
+    }
+    // action performed every tick with a delay of 1 tick.
+    public void actionPerformed(ActionEvent e) {
+        currentTick++;
+    }
+
+    public long getCurrentTick(){
+        return currentTick;
+    }
+
 }
