4525b717611c2867a3d8f549b27b81c8f5b8cd20
wps-dpetty <dpetty@winchesterps.org>
2017-06-16 16:37:21 -0400

    Added back in log4j logging.

--- src/main/java/spaceinvaders/SpaceInvaders.java
+++ src/main/java/spaceinvaders/SpaceInvaders.java
@@ -2,11 +2,9 @@
  * SpaceInvaders.java
  */
 package spaceinvaders;
-
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
-
-import javax.swing.SwingUtilities;
+import org.apache.logging.log4j.*;
+import javax.swing.*;
+import java.util.*;
 
 /**
  * SpaceInvaders is the main class for the SpaceInvaders game.
@@ -14,40 +12,40 @@ import javax.swing.SwingUtilities;
  * @author David C. Petty
  */
 public class SpaceInvaders {
-	/** LONG name of this project. */
-	public static final String LONG = "SpaceInvaders";
-	/** SHORT name of this project. */
-	public static final String SHORT = "SI";
-	/** log4j logger. */
-	private static Logger logger = LogManager.getLogger(SHORT);
+ /** LONG name of this project. */
+ public static final String LONG = "SpaceInvaders";
+ /** SHORT name of this project. */
+ public static final String SHORT = "SI";
+ /** log4j logger. */
+ private static Logger logger = LogManager.getLogger(SHORT);
 
-	/** GUI for <code>SpaceInvaders</code> game. */
-	private static GUI gui;
+ /** GUI for <code>SpaceInvaders</code> game. */
+ private static GUI gui;
 
-	/**
-	 * <code>SpaceInvaders</code> graphical user interface.
-	 *
-	 * @return graphical user interface for <code>SpaceInvaders</code> game.
-	 */
-	public static GUI getGUI() {
-		return gui;
-	}
+ /**
+  * <code>SpaceInvaders</code> graphical user interface.
+  *
+  * @return graphical user interface for <code>SpaceInvaders</code> game.
+  */
+ public static GUI getGUI() {
+  return gui;
+ }
 
-	/**
-	 * SpaceInvaders main method that initializes the entire game.
-	 *
-	 * @param args
-	 *            command-line argument array
-	 */
-	public static void main(String[] args) {
-		logger.info("# SpaceInvaders");
-		SoundFX sound = new SoundFX("sounds/some-days-you-just-can't-get-rid-of-a-bomb.mp3");
-		sound.play("sounds/some-days-you-just-can't-get-rid-of-a-bomb.mp3");
-		SwingUtilities.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				gui = new GUI();
-			}
-		});
-	}
+ /**
+  * SpaceInvaders main method that initializes the entire game.
+  *
+  * @param args
+  *            command-line argument array
+  */
+ public static void main(String[] args) {
+  logger.info("# SpaceInvaders");
+  SoundFX sound = new SoundFX("sounds/some-days-you-just-can't-get-rid-of-a-bomb.mp3");
+  sound.play("sounds/some-days-you-just-can't-get-rid-of-a-bomb.mp3");
+  SwingUtilities.invokeLater(new Runnable() {
+   @Override
+   public void run() {
+    gui = new GUI();
+   }
+  });
+ }
 }
