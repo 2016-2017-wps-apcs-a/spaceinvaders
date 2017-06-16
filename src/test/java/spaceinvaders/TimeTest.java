6b2256622590e5925cd0d0522442aff7576d2b15
Deniz Guler <TheToasterOfDoom1@gmail.com>
2017-06-16 10:02:50 -0400

    Added TimeTest

--- src/test/java/spaceinvaders/TimeTest.java
+++ src/test/java/spaceinvaders/TimeTest.java
@@ -3,5 +3,24 @@
  */
 package spaceinvaders;
 
+
+//import org.junit.Test;
+
 public class TimeTest {
+    //@Test
+    public static void testCurrentTick() {
+        Time test = new Time();
+        //long start = test.getCurrentTick();
+        //while(test.getCurrentTick() == 0L)
+        //    ;
+        //System.out.print("start");
+        while(true)
+                System.out.print(test.getCurrentTick() + ", ");
+        //System.out.print(test.getCurrentTick() + ", ");
+    }
+
+    public static void main(String[] args) {
+        testCurrentTick();
+    }
+
 }
