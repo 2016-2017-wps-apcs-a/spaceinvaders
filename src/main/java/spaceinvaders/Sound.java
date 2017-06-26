6b4f123eb561cfb477a2cc331df7055b696e0ef1
Jennifer (Jaehei) Kim <kimerson@Jennifers-MacBook-Pro.local>
2017-06-26 08:20:44 -0400

    Updated Sound Class

--- src/main/java/spaceinvaders/Sound.java
+++ src/main/java/spaceinvaders/Sound.java
@@ -1,60 +1,25 @@
-/*
- * Sound.java
+/**
+ * Adds sound effects to the overall game
+ * 
+ * @Jennifer Kim, Brandon Zhang
+ * @6/1/17
  */
-package spaceinvaders;
 
 import java.awt.*;
 import java.awt.event.*;
+
 import javax.swing.*;
 
-/**
- * Describe the Sound class...
- *
- * @author Jennifer Kim
- * @author Brandon Zhang
- */
 public class Sound extends JFrame
-     implements ActionListener
 {
-  /** Spaceship sound. */
   private SoundFX spaceship;
   private int time;
+  private String name;
 
-  
-  // fix
-  public Sound()
+  public Sound(String name)
   {
-    super("Morning");
-    
-    time = 0;
-    Timer clock = new Timer(5000, this);
-    clock.start();
-
-    Container c = getContentPane();
-    c.setBackground(Color.WHITE);
-  }
-
-  public static void main(String[] args)
-  {
-    /*SoundFX sound = new SoundFX();
-    SoundFX.setSize(300, 150);
-    SoundFX.setDefaultCloseOperation(EXIT_ON_CLOSE);
-    SoundFX.setVisible(true);*/
-	  }
-  
-  public void actionPerformed(ActionEvent e)
-  {
-    Container c = getContentPane();
-     if (time == 0) 
-    {
-        c.setBackground(Color.BLACK);
-        time += 5000;
-    }
-    
-    else
-    {
-        c.setBackground(Color.WHITE);
-        time = 0;
-    }
+    super("Sound");
+    spaceship = new SoundFX(name);
+    spaceship.play();
   }
 }
